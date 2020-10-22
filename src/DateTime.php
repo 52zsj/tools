e5355f20e005d794745ee897befb8b7669b64e79
傲杰笔记 <1095143545@qq.com>
2020-10-22 18:18:43 +0800

    添加TREE dateTime 类函数

---
+++ src/DateTime.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace aj\tools;
+
+
+class DateTime
+{
+
+    public static function test(){
+        echo 'date_time_test';
+    }
+}
---
+++ src/Tree.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace aj\tools;
+
+
+class Tree
+{
+    public static function test(){
+        echo 'tree_test';
+    }
+}
